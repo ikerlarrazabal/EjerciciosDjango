e04e19e54e15a2b1102af42fad75612f5b85c69e
iker <ikerlarrazabalopendeusto.es>
2025-04-09 15:56:00 +0200

    Cambios antes de seguir guardados

--- mysite/myapp/urls.py
+++ mysite/myapp/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from . import views
 urlpatterns = [
- path('', views.index, name='index'),
-]
+path('', views.index, name='index'),
+ # ej: /miApp/empresas/
+ path('empresas/<str:nombre_empresa>/', views.nombre, name='empresa'),
+ # ej: /miApp/empresa/5/
+ path('empresas/<int:id_empresa>', views.detalle, name='detalle')]
--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from .models import Empresa
+#devuelve el listado de empresas
 def index(request):
- return HttpResponse("Hello, world!")
+ empresas = Empresa.objects.order_by('nombre')
+ output = ', '.join([e.nombre for e in empresas])
+ return HttpResponse(output)
